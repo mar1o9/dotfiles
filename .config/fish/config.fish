064e8d769ccb5019df36121daa0c88e128950709
mar1o9 <144303445+mar1o9@users.noreply.github.com>
2025-07-21 23:25:42 +0300

    new nvim config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,9 +9,10 @@ alias ll='ls -al'
 alias c='clear'
 alias grep='grep --color=auto'
 alias vpm="vpm --color=auto"
+#alias cat="bat"
 
 export QT_QPA_PLATFORMTHEME=qt6ct
 #export WINEPREFIX=/home/mar/.wine32
-export PATH="$HOME/.local/bin:$HOME/.cargo/bin:$PATH"
+export PATH="$HOME/.local/bin:$HOME/.cargo/bin:$HOME/.dotfiles/.config/emacs/bin:$PATH"
 
 eval "$(oh-my-posh init fish --config /usr/share/oh-my-posh/themes/atomic.omp.json)"
